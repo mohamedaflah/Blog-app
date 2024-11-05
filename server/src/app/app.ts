c1d64401801fe0d0ee03d8679ff3f7888bd91de3
Aflu <mohamedaflah186@gmail.com>
2024-11-05 15:21:11 +0530

    feat: some changes

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   })
 );
